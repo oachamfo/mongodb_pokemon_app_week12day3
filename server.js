254e47aaf2cf66eea366f7c7640a0d7fd4ac54d5
“Owusu <owusuachamfour@gmail.com>
2023-09-26 16:05:39 -0400

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -5,16 +5,26 @@ const port = 3000;
 
 const jsxEngine = require("jsx-view-engine"); //require view engine
 const mongoose = require("mongoose"); //require mongoose db
-require("dotenv").config(); //require .env file
 
 //templating engine for views
 app.set("view engine", "jsx");
 app.engine("jsx", jsxEngine());
 
-//app.use
-app.use(methodOverride("_method"));
+//app.use invocations
+
+//models
+const pokemon = require("./models/pokemon.js");
 
 //routes
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
+});
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
+});
+
+//listen on port 3000
+app.listen(port, () => {
+  console.log("listening");
 });
