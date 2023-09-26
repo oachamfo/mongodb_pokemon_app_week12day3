27e94c42271c7c3ea5e37d432ffe8674b25eeafa
“Owusu <owusuachamfour@gmail.com>
2023-09-26 18:38:14 -0400

    Created show views of each pokemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -4,6 +4,11 @@ const myStyle = {
   color: "#ffffff",
   backgroundColor: "#000000",
 };
+const backStyle = {
+  textDecoration: "none",
+  backgroundColor: "yellow",
+  color: "blue",
+};
 
 class Show extends React.Component {
   render() {
@@ -11,10 +16,23 @@ class Show extends React.Component {
     const pokemon_id = this.props.id;
     return (
       <div style={myStyle}>
-        <h1>See A Pokemon!</h1>
-        <ul>
-          <li>The pokemon id: {pokemon_id}. This is the index of an array.</li>
-        </ul>
+        <h1>Gotta Catch 'Em All</h1>
+        <h2>
+          {pokemon?.name.charAt(0).toUpperCase()}
+          {pokemon?.name.slice(1)}
+        </h2>
+        {pokemon?.img ? (
+          <img src={pokemon?.img + ".jpg"}></img>
+        ) : (
+          <p>Sorry, no image.</p>
+        )}
+
+        <br></br>
+        <h1>
+          <a style={backStyle} href="/pokemon">
+            Back
+          </a>
+        </h1>
       </div>
     );
   }
